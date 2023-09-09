bddabf721fc34e3f499333f190131dd02a2edada
Melissa Ho <melissa.h717@outlook.com>
2023-09-09 16:44:25 -0700

    basic setup of rest framework

--- server/music_discovery/user_registration/urls.py
+++ server/music_discovery/user_registration/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+import views
 
-from . import views
+router = routers.DefaultRouter()
+router.register(r'users', views.UsersViewSet)
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

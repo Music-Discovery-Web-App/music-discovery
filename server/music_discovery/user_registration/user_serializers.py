787681ef9802f58361ed9c7d6343f10f9ee842da
Sami Saxton-Getty <67216252+saxgetty@users.noreply.github.com>
2023-09-21 15:47:25 -0700

    Update user_serializers.py typo
    Signed-off-by: Sami Saxton-Getty <67216252+saxgetty@users.noreply.github.com>

--- server/music_discovery/user_registration/user_serializers.py
+++ server/music_discovery/user_registration/user_serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from user_registration.models import UserRegistration
 
-class UserSerializer(serializers.HyperLinkedModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserRegistration
         fields = ["user_id", "first_name", "last_name", "email", "password", "date_created"]

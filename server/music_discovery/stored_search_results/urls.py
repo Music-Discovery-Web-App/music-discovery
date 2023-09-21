888d1aab87919f6565a9b4d772711c5bd5119f1d
Sami <saxgetty4@gmail.com>
2023-09-21 14:55:55 -0700

    Changing urls.py for each feature to include corrected import

--- server/music_discovery/stored_search_results/urls.py
+++ server/music_discovery/stored_search_results/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-import views
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r'results', views.ResultsViewSet)

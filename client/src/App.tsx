dc8edde498b5a65e21de1d1877e326e1b6210447
Melissa Ho <melissa.h717@outlook.com>
2023-09-29 14:48:36 -0700

    Navbar style and mobile style completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import ResultSidebar from './components/ResultSidebar/ResultSidebar'
 
 function App() {
-  const [textInput, setTextInput] = useState<string>("")
-  const [showPost, setShowPost] = useState<boolean>(false)
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const userInput = e.target.value
-    setTextInput(userInput)
-  }
-
-  const shitPost = () => {
-    setShowPost(true)
-  }
   return (
     <>
       <Navbar/>
-      <div className="flex flex-col justify-center items-center">
-        This is just to test the tailwind CSS. Type someone's name below. 
-        <input
-        type="text"
-        placeholder="type something here"
-        className="w-[300px] ml-6 bg-black text-white focus:outline-red"
-        value={textInput}
-        onChange={handleInputChange}
-        />
-        <button onClick={shitPost}> Button here
-          {showPost && <p className="text-xl">{textInput}, why are you gae?</p>}
-        </button>
-      </div>
-      <Footer/>
+      <ResultSidebar />
+      {/* <Footer/> */}
     </>
   )
 }
---
+++ client/src/shared/search.types.ts
@@ -0,0 +1,8 @@
+export type SearchResults = {
+    name: string
+    artist: string
+    genre: string
+    duration: number
+    url: string
+    favorited: boolean
+}

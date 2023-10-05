1e4091b1077a94a8fa799b6035fafe7db4f536bf
Melissa Ho <melissa.h717@outlook.com>
2023-10-04 23:00:07 -0700

    Created result card and added header

---
+++ client/src/views/Home/components/ResultCard.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+//todo: make query to fetch search results, when clicked, display search results on main screen
+
+const ResultCard = () => {
+  return (
+    <div className="flex flex-col font-young-serif w-[100%] p-2">
+      <div className="w-full flex bg-gray-200 h-32 items-center">
+        Results Card
+      </div>
+    </div>
+  )
+}
+
+export default ResultCard
--- client/src/views/Home/components/ResultSidebar.tsx
+++ client/src/views/Home/components/ResultSidebar.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, useEffect, useState } from "react"
+import ResultCard from "./ResultCard"
 // Commented out for now for the sake of Frontend CI
 // import { SearchResults } from "src/shared/search.types"
 
@@ -34,19 +35,26 @@ const ResultSidebar = (): ReactElement => {
         </button>
       )}
       <div
-        className="fixed top-0 right-0 h-full w-64 bg-gray-600 z-50 transform transition-transform duration-200 ease-in-out"
+        className="fixed top-0 right-0 h-full w-72 bg-gray-600 z-50 transform transition-transform duration-200 ease-in-out"
         style={{ transform: isOpen ? "translateX(0)" : "translateX(100%)" }}
       >
         <div className="flex flex-col w-full z-5">
-          <div className="flex items-center p-4">
+          <div className="flex items-center p-2">
             <button
-              className="p-4 text-white"
+              className="p-2 text-white"
               onClick={() => setIsOpen(!isOpen)}
             >
               {isOpen ? "X" : ""}
             </button>
           </div>
         </div>
+        <div className="font-young-serif text-[30px] p-2 text-white">Header goes here</div>
+        {/*map over result data returne dfrom the db, rendering now for clarity*/}
+        <ResultCard />
+        <ResultCard />
+        <ResultCard />
+        <ResultCard />
+        <ResultCard />
       </div>
     </div>
   )

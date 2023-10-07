fbad1a20b8f37991a2de94306cfbb109c40fd2c6
Sami Saxton-Getty <67216252+saxgetty@users.noreply.github.com>
2023-10-07 16:52:10 -0700

    Update routes.tsx to comment out unused footer for CI
    Signed-off-by: Sami Saxton-Getty <67216252+saxgetty@users.noreply.github.com>

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,4 +1,4 @@
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 // import ResultSidebar from "./components/ResultSidebar/ResultSidebar";
 import Home from "./views/Home/Home";

d51f33994617833fc8ad8a83fba341f24e23b474
mattrlee35 <mattrlee35@gmail.com>
2023-10-12 19:34:41 -0500

    created user registration

--- client/src/views/Register/Register.tsx
+++ client/src/views/Register/Register.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import RegistrationForm from "./components/RegistrationForm"
 
-const Register = () => {
+const Registration: React.FC = () => {
+  const navigate = useNavigate()
+
+  const handleRegistrationSuccess = (token: string) => {
+    console.log("Registration successful. Token:", token)
+    navigate("/")
+  }
+
+  const handleRegistrationFailure = (error: string) => {
+    console.error("Registration failed:", error)
+  }
+
   return (
-    <div>This is the Registation Page</div>
+    <div>
+      <h1>Register</h1>
+      <RegistrationForm
+        onRegistrationSuccess={handleRegistrationSuccess}
+        onRegistrationFailure={handleRegistrationFailure}
+      />
+    </div>
   )
 }
 
-export default Register
+export default Registration
--- server/music_discovery/user_registration/views.py
+++ server/music_discovery/user_registration/views.py
@@ -1,9 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from user_registration.serializers import UserRegistrationSerializer, CustomUserSerializer
 from user_registration.models import CustomUser
+from rest_framework.permissions import IsAuthenticated
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
 
 @csrf_exempt
 @api_view(['POST'])
@@ -25,3 +27,17 @@ def login(request):
         serializer = CustomUserSerializer(user) 
         return Response(serializer.data)
     return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+
+    try:
+        token = Token.objects.get(user=request.user)
+    except Token.DoesNotExist:
+        return Response({'detail': 'Token not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+    token.delete()
+
+    return Response({'detail': 'Logged out successfully'}, status=status.HTTP_200_OK)
